d8ed67edce3caa57920eb11707aba5d0a71b0f6e
Simon Johnston <johnstonskj@gmail.com>
2026-03-10 11:18:30 -0700

    style: new header doc formatting

--- gh.plugin.zsh
+++ gh.plugin.zsh
@@ -1,17 +1,11 @@
 # -*- mode: sh; eval: (sh-set-shell "zsh") -*-
 #
-# @name gh
-# @brief Zsh plugin to update completions for 'gh' command.
-# @repository https://github.com/johnstonskj/zsh-gh-plugin
-# @version 0.1.0,
-# @license MIT AND Apache-2.0
+# @name :gh
+# @brief: Update completions for the 'gh' command.
+# @repository: https://github.com/johnstonskj/zsh-gh-plugin
+# @version: 0.1.0,
+# @license: MIT AND Apache-2.0
 #
-#
-# ### State Variables
-#
-# * **PLUGIN**: Plugin-defined global associative array with the following keys:
-#   * **_NAME**: The name of this plugin.
-#   * **_PATH**: The complete file path to the plugin's file.
 #
 
 ############################################################################
@@ -19,9 +13,7 @@
 # @description Standard path and variable setup.
 #
 
-typeset -A PLUGIN
-PLUGIN[_PATH]="$(@zplugins_normalize_zero "$0")"
-PLUGIN[_NAME]="${${PLUGIN[_PATH]:t}%%.*}"
+GH_PLUGIN_PATH="$(@zplugins_normalize_zero "$0")"
 
 ############################################################################
 # @section Lifecycle
@@ -37,7 +29,7 @@ gh_plugin_init() {
     builtin emulate -L zsh
     builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
 
-    @completion_generate_local_file "${PLUGIN[_NAME]}" "${PLUGIN[_PATH]}" gh completion --shell zsh
+    @completion_generate_local_file gh "${GH_PLUGIN_PATH}" gh completion --shell zsh
 }
 
 gh_plugin_unload() {
